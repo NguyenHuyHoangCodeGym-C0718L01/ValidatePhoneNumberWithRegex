3caa594802f5cebcdcb0c3a030f203323a750b69
hoangnhb <hoangnhb@bkav.com>
2018-11-14 12:26:47 +0700

    Hoang finish 'Validate Phone Number With Regex' project.

---
+++ src/com/company/PhoneNumberExample.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberExample {
+    private static final String PHONE_NUMBER_REGEX = "^[(]{1}+[0-9]{2}+[)]{1}+[-]{1}+[(]{1}+[0]{1}+[0-9]{9}+[)]{1}$";
+
+    public PhoneNumberExample() {
+    }
+
+
+    public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
